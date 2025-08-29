998a8243a513d0faab3f3049f0542db111cfc5fd
Neil Bauman <nbauman@rocketmail.com>
2025-08-29 12:35:13 -0300

    Add files via upload

---
+++ lib/supabaseServer.ts
@@ -0,0 +1,15 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createClient = () =>
+  createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) { return cookies().get(name)?.value },
+        set(name: string, value: string, options: CookieOptions) { cookies().set(name, value, options) },
+        remove(name: string, options: CookieOptions) { cookies().set(name, '', { ...options, maxAge: 0 }) },
+      },
+    }
+  )
