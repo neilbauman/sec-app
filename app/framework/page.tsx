b71cc235361ecfbc4bb1726c969d2f1722ddb282
Neil Bauman <nbauman@rocketmail.com>
2025-09-07 10:47:45 -0300

    Update page.tsx

--- app/framework/page.tsx
+++ app/framework/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const revalidate: false | 0 number =0;
 
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
