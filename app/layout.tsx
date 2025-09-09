e567e25b8facc51ba9c1b6965e7dd6f93887f5cc
Neil Bauman <nbauman@rocketmail.com>
2025-09-09 08:41:40 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'    // ← this line must be present
+import './globals.css' // must be here
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
