6f2fd14e2eeef678fa4824f61db7413e0a3c33c7
Neil Bauman <nbauman@rocketmail.com>
2025-09-09 20:25:49 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,12 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        badge: {
-          primary: '#0ea5e9',
-          theme: '#22c55e',
-          subtheme: '#f59e0b'
-        }
-      }
-    }
+    extend: {},
   },
-  plugins: []
-} satisfies Config
+  plugins: [],
+} satisfies Config;
