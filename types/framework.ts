0cd468ae3d9870b60ae43a2dcb4d5e35f81da8f8
Neil Bauman <nbauman@rocketmail.com>
2025-09-10 21:23:22 -0300

    Update framework.ts

--- types/framework.ts
+++ types/framework.ts
@@ -1,25 +1,26 @@
+// /types/framework.ts
 export type Pillar = {
   id: string;
   code: string;
   name: string;
-  description: string;
-  sort_order: number;
+  description?: string | null;
+  sort_order?: number | null;
 };
 
 export type Theme = {
   id: string;
-  pillar_id: string;
+  pillar_id: string; // FK to Pillar.id
   code: string;
   name: string;
-  description: string;
-  sort_order: number;
+  description?: string | null;
+  sort_order?: number | null;
 };
 
 export type Subtheme = {
   id: string;
-  theme_id: string;
+  theme_id: string; // FK to Theme.id
   code: string;
   name: string;
-  description: string;
-  sort_order: number;
+  description?: string | null;
+  sort_order?: number | null;
 };
