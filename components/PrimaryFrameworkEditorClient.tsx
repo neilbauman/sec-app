0e774a8804dc90243daf9ef8e237115aea8f9f2d
Neil Bauman <nbauman@rocketmail.com>
2025-09-15 00:34:12 +0100

    Update PrimaryFrameworkEditorClient.tsx

--- components/PrimaryFrameworkEditorClient.tsx
+++ components/PrimaryFrameworkEditorClient.tsx
@@ -1,30 +1,20 @@
 // components/PrimaryFrameworkEditorClient.tsx
 "use client";
 
-import { Database } from "@/types/supabase";
+import { useState } from "react";
 import PrimaryFrameworkCards from "./PrimaryFrameworkCards";
+import type { FrameworkData } from "@/types/framework";
 
-type Pillar = Database["public"]["Tables"]["pillars"]["Row"];
-type Theme = Database["public"]["Tables"]["themes"]["Row"];
-type Subtheme = Database["public"]["Tables"]["subthemes"]["Row"];
-type Indicator = Database["public"]["Tables"]["indicators"]["Row"];
-
-interface FrameworkData extends Pillar {
-  themes: (Theme & {
-    subthemes: (Subtheme & {
-      indicators: Indicator[];
-    })[];
-  })[];
+interface Props {
+  data: FrameworkData[];
 }
 
-export default function PrimaryFrameworkEditorClient({
-  data,
-}: {
-  data: FrameworkData[];
-}) {
+export default function PrimaryFrameworkEditorClient({ data }: Props) {
+  const [pillars, setPillars] = useState<FrameworkData[]>(data);
+
   return (
     <div className="grid grid-cols-1 gap-6">
-      {data.map((pillar) => (
+      {pillars.map((pillar) => (
         <PrimaryFrameworkCards key={pillar.id} pillar={pillar} />
       ))}
     </div>
