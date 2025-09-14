838d1efe2e83de3f3adbd34d2a11d80a6d0f922e
Neil Bauman <nbauman@rocketmail.com>
2025-09-15 00:55:06 +0100

    Update framework.ts

--- types/framework.ts
+++ types/framework.ts
@@ -1,35 +1,12 @@
 // types/framework.ts
 
-export interface Indicator {
-  id: string;
-  ref_code: string;
-  name: string;
-  description: string;
-  sort_order: number;
-  subtheme_id: string;   // FK
-  subtheme_code?: string;
-}
-
-export interface Subtheme {
-  id: string;
-  ref_code: string;
-  name: string;
-  description: string;
-  sort_order: number;
-  theme_id: string;   // FK
-  theme_code?: string;
-  indicators?: Indicator[];
-}
-
 export interface Theme {
   id: string;
   ref_code: string;
+  pillar_id: string;
   name: string;
   description: string;
   sort_order: number;
-  pillar_id: string;   // FK
-  pillar_code?: string;
-  subthemes?: Subtheme[];
 }
 
 export interface Pillar {
@@ -38,5 +15,5 @@ export interface Pillar {
   name: string;
   description: string;
   sort_order: number;
-  themes?: Theme[];
+  themes: Theme[];
 }
