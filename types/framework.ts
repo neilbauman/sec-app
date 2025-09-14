847cf7b283cf964298c115c5592351d8b4847ca8
Neil Bauman <nbauman@rocketmail.com>
2025-09-15 00:43:54 +0100

    Update framework.ts

--- types/framework.ts
+++ types/framework.ts
@@ -3,10 +3,10 @@
 export interface Indicator {
   id: string;
   ref_code: string;
-  subtheme_id: string | null;
-  theme_id: string | null;
+  subtheme_id: string;
+  subtheme_code: string;
   name: string;
-  description?: string;   // optional
+  description: string;
   sort_order: number;
 }
 
@@ -14,30 +14,36 @@ export interface Subtheme {
   id: string;
   ref_code: string;
   theme_id: string;
+  theme_code: string;
   name: string;
-  description?: string;   // optional
+  description: string;
   sort_order: number;
-  indicators: Indicator[];
+  indicators?: Indicator[];
 }
 
 export interface Theme {
   id: string;
   ref_code: string;
   pillar_id: string;
+  pillar_code: string;
   name: string;
-  description?: string;   // optional
+  description: string;
   sort_order: number;
-  subthemes: Subtheme[];
-  indicators: Indicator[];
+  subthemes?: Subtheme[];
+  indicators?: Indicator[]; // themes can also have indicators
 }
 
 export interface Pillar {
   id: string;
   ref_code: string;
   name: string;
-  description?: string;   // optional
+  description: string;
   sort_order: number;
-  themes: Theme[];
+  themes?: Theme[];
 }
 
+/**
+ * Used for flexible passing of nested data in components like
+ * `PrimaryFrameworkEditorClient` and `PrimaryFrameworkCards`.
+ */
 export type FrameworkData = Pillar;
