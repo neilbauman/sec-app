2550dd003ef854403a5369c095a47693bf0464fa
Neil Bauman <nbauman@rocketmail.com>
2025-09-16 20:49:29 -0300

    Delete app/ssc directory

--- app/ssc/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SscManagementPage() {
-  return (
-    <div className="space-y-4">
-      <h2 className="text-xl font-semibold">SSC Instance Management</h2>
-      <p className="text-gray-600">Coming soon: manage SSC instances.</p>
-    </div>
-  );
-}
