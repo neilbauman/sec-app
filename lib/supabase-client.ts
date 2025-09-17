a0980043facba902d63371918000a04248d3d52b
Neil Bauman <nbauman@rocketmail.com>
2025-09-17 12:00:20 -0300

    Update supabase-client.ts

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,9 +1,8 @@
 // /lib/supabase-client.ts
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase";
 
-export const createBrowserSupabase = () => {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // anon key for client-side
-  );
-};
+export const supabaseClient = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
