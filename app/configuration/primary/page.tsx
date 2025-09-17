654fecfa01680492e3d781daa021838e5d94764b
Neil Bauman <nbauman@rocketmail.com>
2025-09-17 06:53:46 -0300

    Create page.tsx

---
+++ app/configuration/primary/page.tsx
@@ -0,0 +1,9 @@
+import PrimaryFrameworkEditorClient from "@/components/ui/PrimaryFrameworkEditorClient";
+
+export default function PrimaryFrameworkPage() {
+  return (
+    <main className="p-6 space-y-6">
+      <PrimaryFrameworkEditorClient />
+    </main>
+  );
+}
