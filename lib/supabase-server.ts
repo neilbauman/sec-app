e631f4c73ef5b553e416e439fca7b86ed6f564c4
Neil Bauman <nbauman@rocketmail.com>
2025-09-17 10:45:34 -0300

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,11 +1,9 @@
 // /lib/supabase-server.ts
-import { cookies } from "next/headers";
-import { createServerClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
 export const createServerSupabase = () => {
-  return createServerClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    process.env.SUPABASE_SERVICE_ROLE_KEY! // service key for server-side only
   );
 };
