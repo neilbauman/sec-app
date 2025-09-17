f21a7b9d13518c59c501c8a692cf0c9c279ff9cc
Neil Bauman <nbauman@rocketmail.com>
2025-09-17 10:47:25 -0300

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -4,6 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 export const createServerSupabase = () => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY! // service key for server-side only
+    process.env.SUPABASE_SERVICE_ROLE_KEY! // service role key for secure server access
   );
 };
