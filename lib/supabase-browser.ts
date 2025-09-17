e243e8842ab50e6e84ac68cbf27ec9ca98f20f40
Neil Bauman <nbauman@rocketmail.com>
2025-09-17 15:24:20 -0300

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,8 +1,8 @@
 // /lib/supabase-browser.ts
 import { createBrowserClient } from "@supabase/ssr";
-import { Database } from "@/types/supabase";
+import type { Database } from "@/types/supabase";
 
-export const supabaseBrowser = createBrowserClient<Database>(
+export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
