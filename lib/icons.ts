5d875fc0557f26fb92956c9f71c8cc97482cff8e
Neil Bauman <nbauman@rocketmail.com>
2025-09-18 18:35:37 -0300

    Create icons.ts

---
+++ lib/icons.ts
@@ -0,0 +1,11 @@
+// /lib/icons.ts
+import { Info, Cog, Globe, Database, FileText } from "lucide-react";
+
+export const groupIcons = {
+  about: { icon: Info, color: "bg-blue-600" },
+  configuration: { icon: Cog, color: "bg-green-600" },
+  country: { icon: Globe, color: "bg-yellow-600" },
+  instances: { icon: Database, color: "bg-purple-600" },
+};
+
+export const editorIcon = { icon: FileText, color: "bg-green-600" };
