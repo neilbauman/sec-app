81ecc50e6068867bf04aa3869fb01df9edc0a520
Neil Bauman <nbauman@rocketmail.com>
2025-09-18 18:39:41 -0300

    Update icons.ts

--- lib/icons.ts
+++ lib/icons.ts
@@ -2,10 +2,10 @@
 import { Info, Cog, Globe, Database, FileText } from "lucide-react";
 
 export const groupIcons = {
-  about: { icon: Info, color: "bg-blue-600" },
-  configuration: { icon: Cog, color: "bg-green-600" },
-  country: { icon: Globe, color: "bg-yellow-600" },
-  instances: { icon: Database, color: "bg-purple-600" },
+  about: { icon: Info, iconColor: "bg-blue-600" },
+  configuration: { icon: Cog, iconColor: "bg-green-600" },
+  country: { icon: Globe, iconColor: "bg-yellow-600" },
+  instances: { icon: Database, iconColor: "bg-purple-600" },
 };
 
-export const editorIcon = { icon: FileText, color: "bg-green-600" };
+export const editorIcon = { icon: FileText, iconColor: "bg-green-600" };
