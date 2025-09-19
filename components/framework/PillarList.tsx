f3094af367db27fd11564757999837f304a321d8
Neil Bauman <nbauman@rocketmail.com>
2025-09-19 09:15:40 -0300

    Update PillarList.tsx

--- components/framework/PillarList.tsx
+++ components/framework/PillarList.tsx
@@ -1,16 +1,2 @@
-'use client';
-import ThemeList from './ThemeList';
-
-export default function PillarList() {
-  // TODO: fetch pillars from Supabase and map them here
-  return (
-    <div>
-      <h2 className="text-xl font-semibold">Pillars</h2>
-      <div className="border p-2 rounded bg-white shadow-sm">
-        {/* Example placeholder */}
-        <div className="p-2 border-b">Pillar 1: The Shelter</div>
-        <ThemeList />
-      </div>
-    </div>
-  );
-}
+// Deprecated - superseded by FrameworkEditor
+export default function PillarList(){return null}
