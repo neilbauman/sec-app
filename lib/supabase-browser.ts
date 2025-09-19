dfcc072b19cf009c6ff14a51ee3a68d615129c45
Neil Bauman <nbauman@rocketmail.com>
2025-09-19 18:04:40 -0300

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,8 +1,13 @@
-// /lib/supabase-browser.ts
+// lib/supabase-browser.ts
 import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "@/types/supabase";
 
+/**
+ * Create a new Supabase client for use in the browser (React components).
+ * This function should be called only on the client.
+ */
 export function createClient() {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
