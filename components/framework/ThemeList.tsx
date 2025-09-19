fd0deae5d056f3c333144888a635fe1e7e3ce984
Neil Bauman <nbauman@rocketmail.com>
2025-09-19 09:16:42 -0300

    Update ThemeList.tsx

--- components/framework/ThemeList.tsx
+++ components/framework/ThemeList.tsx
@@ -1,16 +1,2 @@
-'use client';
-import SubthemeList from './SubthemeList';
-
-export default function ThemeList() {
-  // TODO: fetch themes from Supabase and map them here
-  return (
-    <div className="ml-4">
-      <h3 className="text-lg font-medium">Themes</h3>
-      <div className="border p-2 rounded bg-gray-50">
-        {/* Example placeholder */}
-        <div className="p-2 border-b">Theme 1.1: Physical Safety</div>
-        <SubthemeList />
-      </div>
-    </div>
-  );
-}
+// Deprecated placeholder
+export default function ThemeList(){return null}
