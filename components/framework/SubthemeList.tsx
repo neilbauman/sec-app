64b582b99b0a90e607e6a32df4024d3d646aec80
Neil Bauman <nbauman@rocketmail.com>
2025-09-19 09:16:09 -0300

    Update SubthemeList.tsx

--- components/framework/SubthemeList.tsx
+++ components/framework/SubthemeList.tsx
@@ -1,14 +1,2 @@
-'use client';
-
-export default function SubthemeList() {
-  // TODO: fetch subthemes from Supabase and map them here
-  return (
-    <div className="ml-8">
-      <h4 className="font-medium">Subthemes</h4>
-      <ul className="list-disc pl-4">
-        {/* Example placeholder */}
-        <li>Subtheme 1.1.1: Secure Shelter</li>
-      </ul>
-    </div>
-  );
-}
+// Deprecated placeholder
+export default function SubthemeList(){return null}
