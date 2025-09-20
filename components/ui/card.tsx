6ae7538aecd906fa1cfda99f8ed4767390abb974
Neil Bauman <nbauman@rocketmail.com>
2025-09-20 15:15:35 -0300

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -3,3 +3,15 @@ import * as React from "react";
 export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return <div className={`rounded border bg-white shadow ${className}`} {...props} />;
 }
+
+export function CardHeader({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`border-b px-4 py-2 font-semibold ${className}`} {...props} />;
+}
+
+export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`px-4 py-2 ${className}`} {...props} />;
+}
+
+export function CardFooter({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`border-t px-4 py-2 ${className}`} {...props} />;
+}
