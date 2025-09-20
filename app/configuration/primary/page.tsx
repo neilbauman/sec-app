6ae309a3855c7b5f2b11e10986699cabb7a5ebfb
Neil Bauman <nbauman@rocketmail.com>
2025-09-20 13:36:43 -0300

    Update page.tsx

--- app/configuration/primary/page.tsx
+++ app/configuration/primary/page.tsx
@@ -1,10 +1,6 @@
 // app/configuration/primary/page.tsx
 import FrameworkEditor from "@/components/framework/FrameworkEditor";
-import { fetchFramework } from "@/lib/framework-client";
 
-export default async function PrimaryFrameworkEditorPage() {
-  // Fetch the data server-side
-  const pillars = await fetchFramework();
-
-  return <FrameworkEditor data={pillars} />;
+export default function PrimaryFrameworkEditorPage() {
+  return <FrameworkEditor />;
 }
