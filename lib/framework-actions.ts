1edadb056b7339fce59992b386bc721dca065667
Neil Bauman <nbauman@rocketmail.com>
2025-09-20 11:37:30 -0300

    Update framework-actions.ts

--- lib/framework-actions.ts
+++ lib/framework-actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/lib/supabase-server";
 
 // -------- Pillars --------
 export async function addPillar(data: {
