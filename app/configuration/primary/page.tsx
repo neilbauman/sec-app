dc7c2a687f320620fce23caa5d051e28ad7e2bc3
Neil Bauman <nbauman@rocketmail.com>
2025-09-20 10:38:41 -0300

    Update page.tsx

--- app/configuration/primary/page.tsx
+++ app/configuration/primary/page.tsx
@@ -1,9 +1,6 @@
+// app/configuration/primary/page.tsx
 import FrameworkEditor from "@/components/framework/FrameworkEditor";
-import { fetchFramework } from "@/lib/framework-client";
 
-export default async function PrimaryFrameworkEditorPage() {
-  // Fetch pillars with themes + subthemes
-  const pillars = await fetchFramework();
-
-  return <FrameworkEditor data={pillars} />;
+export default function PrimaryFrameworkEditorPage() {
+  return <FrameworkEditor />;
 }
