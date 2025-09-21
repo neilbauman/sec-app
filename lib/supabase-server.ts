cc802dbbac9929fa58eeaef869837af8fe9aea26
Neil Bauman <nbauman@rocketmail.com>
2025-09-21 20:04:24 -0300

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -2,11 +2,10 @@
 import { createServerClient } from "@supabase/ssr";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
-import type { Database } from "@/types/supabase";
 
 // Create a server-side Supabase client with cookie wrappers
 export function createClient(): SupabaseClient<any> {
-  return createServerClient<Database>(
+  return createServerClient<any>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
