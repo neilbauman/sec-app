6a28166d8eb206ea13b36071a1e54e6abdbf4517
Neil Bauman <nbauman@rocketmail.com>
2025-09-24 15:44:55 -0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,30 @@
 // lib/types.ts
 
+export type NestedSubtheme = {
+  id: string;
+  name: string;
+  description?: string;
+  refCode?: string;
+  sortOrder?: number;
+  type: "Subtheme";
+};
+
+export type NestedTheme = {
+  id: string;
+  name: string;
+  description?: string;
+  refCode?: string;
+  sortOrder?: number;
+  type: "Theme";
+  children?: NestedSubtheme[];
+};
+
 export type NestedPillar = {
   id: string;
   name: string;
   description?: string;
   refCode?: string;
   sortOrder?: number;
-  type: "Pillar" | "Theme" | "Subtheme"; // required for rendering
-  children?: NestedPillar[];
+  type: "Pillar";
+  children?: NestedTheme[];
 };
